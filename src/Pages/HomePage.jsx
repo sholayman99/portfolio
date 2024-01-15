c1847348e265db4a55a867d805e88c67c2494289
Md Sholayman <mdsholayman998@gmail.com>
2024-01-15 22:04:33 +0600

    initialize the project

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppNavbar from "../Components/AppNavbar.jsx";
+
+const HomePage = () => {
+    return (
+        <>
+         <AppNavbar />   
+        </>
+    );
+};
+
+export default HomePage;
